8d1def6bdd13176e8493e347ec28a859f40d9276
Zhang Lihua <zhang.lihua@wanglutech.com>
2017-10-27 16:15:14 +0800

    update faucet

--- util/ethutil.js
+++ util/ethutil.js
@@ -6,15 +6,23 @@ var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
 let coinbase = "0x2d0e7c0813a51d3bd1d08246af2a8a7a57d8922e";
 let coinbaseSK = process.env.COINBASESK;
-console.log(coinbaseSK)
+let g_int_serial  = web3.eth.getTransactionCount(coinbase);
+let g_tx_index = 0;
+
+
+        let filter = web3.eth.filter('latest');
+        filter.watch(function(err,blockhash){
+            g_int_serial  = web3.eth.getTransactionCount(coinbase);
+            g_tx_index = 0;
+        });
 exports.giveEtherTo = function (receiverAddress, etherAmout=1)
 {
-    var int_serial  = web3.eth.getTransactionCount(coinbase);
     var privateKey = new Buffer(coinbaseSK,'hex');
     var amount = web3.toWei(etherAmout, 'ether');
     var bn = new web3.BigNumber(amount);
     var hexValue = '0x' + bn.toString(16);
-    var serial = web3.toHex(int_serial);
+    var serial = web3.toHex(g_int_serial+g_tx_index);
+    g_tx_index += 1;
     var rawTx = {
         nonce: serial,
         gasLimit: '0x100000',
