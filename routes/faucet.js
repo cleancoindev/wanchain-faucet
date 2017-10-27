03dbd56feb2fc03d665f804cc57d052c6bf92f60
Zhang Lihua <zhang.lihua@wanglutech.com>
2017-10-27 19:26:44 +0800

    update faucet

--- routes/faucet.js
+++ routes/faucet.js
@@ -6,7 +6,7 @@ var router = express.Router();
 router.post('/', function(req, res, next) {
   var userAddr = req.body.userAddr;
   console.log(userAddr);
-  let hash = ethUtil.giveEtherTo(userAddr, 1);
+  let hash = ethUtil.giveEtherTo(userAddr, 12);
   if(!hash){
       res.status(503).send("failed to faulet, please try again later");
   }else{
